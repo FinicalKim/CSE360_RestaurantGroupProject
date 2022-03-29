5d91946e55ad0104b4b09906caca7ceaca036091
TJohnsonSE <tylerjohnsonSE@outlook.com>
2022-03-29 12:03:09 -0700

    Fixed a few things such as the EventHandler<Action> --> EventHandler<ActionEvent> and a few others

--- Main_test.java
+++ Main_test.java
@@ -1,17 +1,19 @@
 import java.io.*;
-import java.util.*;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-public class Main_test extends Application implements EventHandler<Action>
+public class Main_test extends Application implements EventHandler<ActionEvent>
 {
 	Stage window;
-	Stage scene1, scene2;
+	Scene scene1, scene2;
 	
 	public static void main(String[] args) throws IOException
 	{
@@ -23,7 +25,7 @@ public class Main_test extends Application implements EventHandler<Action>
 		InputStreamReader isr = new InputStreamReader(System.in);
 		BufferedReader stdin = new BufferedReader(isr);
 		
-		System.out.println("Hello, let's create a User object.");
+		System.out.println("Hello, let's create a new User.");
 		System.out.print("First, type the username you want the user to have: ");
 		inputName = stdin.readLine();
 		System.out.println("Great, you chose '" + inputName + "' as a name.");
@@ -36,11 +38,12 @@ public class Main_test extends Application implements EventHandler<Action>
 	
 	public void start(Stage primaryStage) throws Exception {
 		window = primaryStage;
-		window.setTitle("Restraurant");
+		window.setTitle("Restaurant");
 		Label label1 = new Label("Welcome to the Restaurants application");
-			// Button 1
+
+		// Button 1
 		Button button1 = new Button("Go to Account information");
-		button.setText("Create new User");
+		button1.setText("Create new User");
 		button1.setOnAction(e -> window.setScene(scene2));
 		
 			// Layout 1
@@ -50,16 +53,23 @@ public class Main_test extends Application implements EventHandler<Action>
 		
 			// Button 2
 		Button button2 = new Button("Go to Account information");
-		button.setText("Go back to main menu");
+		button2.setText("Go back to main menu");
 		button2.setOnAction(e -> window.setScene(scene1));
 		
 			// Layout example
 		StackPane layout = new StackPane();
-		layout.getChildren().add(button);
+		layout.getChildren().add(button2);
 		
 		Scene scene = new Scene(layout, 300, 250);
 		window.setScene(scene);
 		window.show();
+
+	}
+
+	@Override
+	public void handle(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
 

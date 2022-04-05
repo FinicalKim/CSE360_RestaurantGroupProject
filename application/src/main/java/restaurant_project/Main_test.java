6ba52f7dbcd07bc7ddd783d7eadc2c5486f8177e
TJohnsonSE <tylerjohnsonSE@outlook.com>
2022-04-04 23:57:57 -0700

    Set up a Create Account page, where a user can enter in their information to create a new User object with said info.  Need to implement the User object creation next

--- application/src/main/java/restaurant_project/Main_test.java
+++ application/src/main/java/restaurant_project/Main_test.java
@@ -13,19 +13,15 @@ import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class Main_test extends Application implements EventHandler<ActionEvent> {
 	Stage window;
-	Scene scene1, scene2;
-	User newUser;
+	Scene scene1, scene2, scene3, scene4;
 
 	public static void main(String[] args) throws IOException {
 
@@ -51,98 +47,177 @@ public class Main_test extends Application implements EventHandler<ActionEvent> 
 
 	public void start(Stage primaryStage) throws Exception {
 
-		window = primaryStage; //main display window
-		window.setMaximized(true); //set the application to open in full-screen mode
+		// Display Window Set-Up
+		// ----------------------
+		window = primaryStage; // main display window
 
 		// Title the window "OrderUp"
 		window.setTitle("OrderUp");
 
-		/*
-		Label label1 = new Label("Welcome to OrderUp, please sign in to get started!");
+		int defaultWindowHeight = 750;
+		int defaultWindowWidth = 750;
 
-		// Button 1
-		Button signInButton = new Button("Go to sign in menu");
-		signInButton.setText("Sign in");
-		signInButton.setOnAction(e -> window.setScene(scene2)); // button 1 sends you to the Sign in page
+		// ---------------------------------------------------
 
-		// Layout 1
-		BorderPane mainBorderPane = new BorderPane(); //the main Pane on which other panes can be drawn
-		mainBorderPane.setCenter(signInButton);
-		BorderPane.setAlignment(signInButton, Pos.CENTER);
-		mainBorderPane.setTop(label1);
-		BorderPane.setAlignment(label1, Pos.CENTER);
-		mainBorderPane.setPadding(new Insets(15,12,15,12));
+		// Scene 1
+		// ---------------
 
-		scene1 = new Scene(mainBorderPane, 300, 300);
-		*/
-//-------------------------------------------------------------------
+		// Panes
+		BorderPane signInPane = new BorderPane(); // the main pane
+		HBox greetingPane = new HBox(); // the greeting at the top of the main 'signInPane' pane
+		VBox centerVBox = new VBox(); // pane to hold the username and password labels and text fields
+		HBox underButtons = new HBox(); // pane to hold buttons displayed underneath text fields
 
-		// Layout 2 - Scene 2
-		BorderPane signInPane = new BorderPane(); //the sign-in page pane
-		HBox greetingPane = new HBox();
+		signInPane.setTop(greetingPane);
+		signInPane.setCenter(centerVBox);
+
+		// Sign-In Button
+		Button signInButton = new Button("Sign in");
+
+		// Create Account Button
+		Button createAccountButton = new Button("Create Account");
+		createAccountButton.setOnAction(e -> window.setScene(scene2));
+		signInPane.getChildren().addAll(signInButton, createAccountButton);
+
+		// Header/Greeting Area
 		greetingPane.setAlignment(Pos.CENTER);
-		HBox bottomButtons = new HBox();
-		VBox centerVBox = new VBox();
-
-		FlowPane centerFlowPane = new FlowPane();
-		centerFlowPane.setPadding(new Insets(5,5,5,5));
-		centerFlowPane.setVgap(4);
+		greetingPane.setPadding(new Insets(200, 0, -150, 0));
 
 		Label greeting = new Label("Welcome to OrderUp, please sign in to get started!");
 		greeting.setFont(Font.font("Calibri", FontWeight.NORMAL, 20));
+		greetingPane.getChildren().addAll(greeting);
+
+		// Central Login Area
+		underButtons.setAlignment(Pos.BOTTOM_CENTER);
+		underButtons.setSpacing(10);
+		underButtons.setPadding(new Insets(20, 0, 30, 0));
+		underButtons.getChildren().addAll(signInButton, createAccountButton);
 
 		Label userName = new Label("User Name:");
-		Label pw = new Label("Password:");
-
 		TextField userTextField = new TextField();
 		userTextField.setPrefWidth(150);
 		userTextField.setMaxWidth(150);
 
+		Label pw = new Label("Password:");
 		PasswordField pwBox = new PasswordField();
 		pwBox.setPrefWidth(150);
 		pwBox.setMaxWidth(150);
 
-		//Sign-In Button 2
-		Button signInButton2 = new Button("Sign in");
-
-		//Main-Menu Button
-		Button mainMenuButton = new Button("Go to Home page");
-		mainMenuButton.setText("Go back to main menu");
-		mainMenuButton.setOnAction(e -> window.setScene(scene1)); // When button 2 is clicked it send you back to the home page
-		signInPane.getChildren().addAll(mainMenuButton, signInButton2);
-
-		//Node alignment and display settings for 'signInPane'
-		greetingPane.getChildren().addAll(greeting);
-		signInPane.setTop(greetingPane);
-		signInPane.setCenter(centerVBox);
-		signInPane.setBottom(bottomButtons);
-
-		//Node alignment and display settings for 'centerVBox'
-		centerVBox.getChildren().addAll(userName, userTextField, pw, pwBox);
+		centerVBox.getChildren().addAll(userName, userTextField, pw, pwBox, underButtons);
 		centerVBox.setAlignment(Pos.CENTER);
 
-		scene2 = new Scene(signInPane, 300, 300);
-
-		// Information to sign in on layout 2
-		bottomButtons.setAlignment(Pos.BOTTOM_CENTER);
-		bottomButtons.getChildren().add(signInButton2);
-
-		final Text actiontarget = new Text();
-
-		signInButton2.setOnAction(e -> 
+		//Print text fields input to standard output when clicking on 'signInButton'
+		signInButton.setOnAction(e -> 
 		{
-
-			actiontarget.setFill(Color.FIREBRICK);
-			actiontarget.setText("Sign in button pressed");
-
-			if (newUser.verifyCredentials(newUser.getUserID(), newUser.getPassword()))
-			{
-				newUser.setLoginStatus(true);
-			}
+			System.out.println(userTextField.getText());
+			System.out.println(pwBox.getText());
 		});
 
+		// Display 'scene1' with the default window size
+		scene1 = new Scene(signInPane, defaultWindowWidth, defaultWindowHeight);
+
+		// End Scene 1
+
+		// Scene 2
+		// -----------------
+
+		// Panes
+		BorderPane createAccountPane = new BorderPane(); //Main pane for 'scene2'
+		VBox createAccountCentralVBox = new VBox();
+		HBox createAccountGreeting = new HBox();
+		HBox underButtons2 = new HBox();
+
+		createAccountPane.setTop(createAccountGreeting);
+		createAccountPane.setCenter(createAccountCentralVBox);
+
+		// Submit Button
+		Button submitButton = new Button("Submit");
+
+		// Main-Menu Button
+		Button mainMenuButton = new Button("Main Menu");
+		mainMenuButton.setOnAction(e -> window.setScene(scene1));
+
+		underButtons2.getChildren().addAll(submitButton, mainMenuButton);
+		underButtons2.setSpacing(10);
+		underButtons2.setAlignment(Pos.CENTER);
+		underButtons2.setPadding(new Insets(20,0,0,0));
+
+		// Header/Greeting Area
+		createAccountGreeting.setAlignment(Pos.CENTER);
+		createAccountGreeting.setPadding(new Insets(200, 0, -150, 0));
+		Label accountGreeting = new Label("Create a New Account");
+		accountGreeting.setFont(Font.font("Calibri", FontWeight.BOLD, 20));
+		createAccountGreeting.getChildren().add(accountGreeting);
+
+		// Central Account Creation Area
+		Label usernameInstruct = new Label("Please choose a username");
+		TextField userTextField2 = new TextField();
+		userTextField2.setMaxWidth(150);
+
+		Label passwordInstruct = new Label("Please choose a password for your account");
+		PasswordField pw2 = new PasswordField();
+		pw2.setMaxWidth(150);
+
+		Label contactName = new Label("Please enter your first and last name");
+		TextField contactNameField = new TextField();
+		contactNameField.setMaxWidth(150);
+
+		Label emailAddress = new Label("Please enter a contact email");
+		TextField emailAddressField = new TextField();
+		emailAddressField.setMaxWidth(150);
+
+		Label phoneNumber = new Label("Please enter your phone-number");
+		TextField phoneNumberField = new TextField();
+		phoneNumberField.setMaxWidth(150);
+
+		createAccountCentralVBox.getChildren().addAll(usernameInstruct, userTextField2, passwordInstruct, pw2,
+				contactName, contactNameField,
+				emailAddress, emailAddressField, phoneNumber, phoneNumberField,underButtons2);
+
+		createAccountCentralVBox.setAlignment(Pos.CENTER);
+
+		scene2 = new Scene(createAccountPane, defaultWindowWidth, defaultWindowHeight);
+
+		// End Scene 2
+
+		/*
+		// Scene 3
+		// ----------
+
+		// Panes
+
+		GridPane layout3 = new GridPane();
+		layout3.setAlignment(Pos.CENTER);
+		layout3.setHgap(10);
+		layout3.setVgap(10);
+		layout3.setPadding(new Insets(25, 25, 25, 25));
+		layout3.getChildren().addAll(button3);
+		scene3 = new Scene(layout3, 300,300);
+		
+		Label total = new Label("Total: $");
+		layout3.add(total, 0, 1);
+		
+		// Button 4
+		Button button4 = new Button("Go to Ingredients page");
+		button4.setText("Ingredients");
+		button4.setOnAction(e -> window.setScene(scene4));
+		
+		// Layout 4
+		GridPane layout4 = new GridPane();
+		layout4.setAlignment(Pos.CENTER);
+		layout4.setHgap(10);
+		layout4.setVgap(10);
+		layout4.setPadding(new Insets(25, 25, 25, 25));
+		layout4.getChildren().addAll(button4);
+		scene4 = new Scene(layout4, 300,300);
+	
+		Label total = new
+		Label("Breakfast Sandwich: /nBiscuit /nCheese /nEgg /nSausage /nButter");
+		layout4.add(total, 0, 1);
+		*/
+
 		// Display the first scene at program launch
-		window.setScene(scene2);
+		window.setScene(scene1);
 		window.show();
 
 	}

ef77601efa309b2f390d66788dafd5d8d4129ae8
TJohnsonSE <tylerjohnsonSE@outlook.com>
2022-04-07 22:56:39 -0700

    Small implementations of the 'Cart' class;  currently working on getting it functioning

--- application/src/main/java/restaurant_project/Cart.java
+++ application/src/main/java/restaurant_project/Cart.java
@@ -1,5 +1,21 @@
 package restaurant_project;
 
+import java.util.ArrayList;
+
 public class Cart {
     
+    private String cartID;
+    private ArrayList<Food> cartItems = new ArrayList<Food>();
+    private double subtotal;
+
+    // Default 'Cart' object constructor
+    public Cart(){
+        cartID = "default_cart_id";
+        subtotal = 0.00;
+    }
+
+    public void removeItem(String foodID)
+    {
+        
+    }
 }

f9f917b40c90a4bd9b2e50f4fe4d53ca66bd2f9b
TJohnsonSE <tylerjohnsonSE@outlook.com>
2022-04-08 12:22:51 -0700

    Implemented a few different methods for the Cart class

--- application/src/main/java/restaurant_project/Cart.java
+++ application/src/main/java/restaurant_project/Cart.java
@@ -1,6 +1,7 @@
 package restaurant_project;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
 
 public class Cart {
     
@@ -14,8 +15,50 @@ public class Cart {
         subtotal = 0.00;
     }
 
+    // Mutator methods
+    public void setCartID(String cartID){
+
+        this.cartID = cartID;
+
+    }
+
+    public void setSubtotal(double subtotal){
+
+        this.subtotal = subtotal;
+
+    }
+
+    // Accessor methods
+    public String getCartID(){
+
+        return this.cartID;
+
+    }
+
+    public double getSubtotal(){
+
+        return this.subtotal;
+
+    }
+
+    // Add a 'Food' object to the list of 'Food' items in the given 'Cart'
+    public void addItem(String foodID, Cart cart) {
+
+        cart.cartItems.add(new Food("Banana", foodID));
+        
+    }
+
+    // Method 'removeItem()' takes a String argument and then uses an ArrayList iterator to iterate through a list of Food objects
+    // and compare the argument to the 'foodID' attribute of each Food.  If the strings match, the method will remove the object
+    // from the ArrayList.
     public void removeItem(String foodID)
     {
-        
+        ListIterator<Food> iterator = cartItems.listIterator();
+        while (iterator.hasNext()){
+            if (iterator.next().getFoodID().compareTo(foodID) == 0){
+                iterator.remove();
+                return;
+            }
+        }
     }
 }
